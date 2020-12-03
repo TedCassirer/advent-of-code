44eaa628b53a439f4f5fbe1342ffed0413d8dabc
Ted Cassirer <ted.cassirer@gmail.com>
2020-12-03 07:36:11 +0100

    Day 3

---
+++ aoc_cas/aoc2020/day3.py
@@ -0,0 +1,22 @@
+import math
+
+
+def traverse(forest, right, down):
+    Y, X = len(forest), len(forest[0])
+    xi = 0
+    trees = 0
+    for yi in range(0, Y, down):
+        trees += forest[yi][xi] == "#"
+        xi = (xi + right) % X
+    return trees
+
+
+def part1(data):
+    forest = data.split("\n")
+    return traverse(forest, 3, 1)
+
+
+def part2(data):
+    forest = data.split("\n")
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    return math.prod(traverse(forest, right, down) for right, down in slopes)
