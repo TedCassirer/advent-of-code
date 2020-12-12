5c775300fe55590cffef6e5b93399796ea51c8cb
Ted Cassirer <ted.cassirer@gmail.com>
2020-12-12 12:30:59 +0100

    Day 11 cleaned up, kinda

--- aoc_cas/aoc2020/day11.py
+++ aoc_cas/aoc2020/day11.py
@@ -3,75 +3,87 @@ EMPTY = "L"
 OCCUPIED = "#"
 
 
-def getNearbySeats(y, x, Y, X, grid, floorOk=True):
-    for dy in (-1, 0, 1):
-        for dx in (-1, 0, 1):
-            if dx == 0 and dy == 0:
-                continue
-            iy = y + dy
-            ix = x + dx
-            while 0 <= iy < Y and 0 <= ix < X:
-                if grid[iy][ix] == FLOOR:
-                    if floorOk:
-                        yield iy, ix
-                        break
-                    iy += dy
-                    ix += dx
-                    continue
-                else:
-                    yield iy, ix
-                    break
-
-
-def tick(grid, floorOk=True):
+def tick(grid, rule, seatsToConnected):
     Y, X = len(grid), len(grid[0])
-    newGrid = []
-    occupiedSeats = 0
-    for y, row in enumerate(grid):
-        newRow = []
-        for x, tile in enumerate(row):
-            if tile == FLOOR:
-                newRow.append(tile)
-                continue
-            nearbySeats = [grid[iy][ix] for iy, ix in getNearbySeats(y, x, Y, X, grid, floorOk)]
-            # print(nearbySeats)
-            occupied = nearbySeats.count(OCCUPIED)
-            if tile == EMPTY:
-                tile = OCCUPIED if occupied == 0 else EMPTY
-            elif tile == OCCUPIED:
-                tile = EMPTY if occupied >= (5 - floorOk) else OCCUPIED
-            else:
-                tile = FLOOR
-            occupiedSeats += tile == OCCUPIED
-            newRow.append(tile)
-        newGrid.append("".join(newRow))
-    return newGrid, occupiedSeats
+    newGrid = [row.copy() for row in grid]
+    totalSeatOccupied = 0
+    for coord, connected in seatsToConnected.items():
+        nearbyOccupied = sum(grid[y][x] == OCCUPIED for y, x in connected)
+        currentlyOccupied = grid[coord[0]][coord[1]] == OCCUPIED
+        updatedSeat = rule(currentlyOccupied, nearbyOccupied)
+        newGrid[coord[0]][coord[1]] = updatedSeat
+        totalSeatOccupied += updatedSeat == OCCUPIED
+
+    return newGrid, totalSeatOccupied
 
 
 def part1(data):
-    grid = data.splitlines()
+    grid = [list(row) for row in data.splitlines()]
+    Y, X = len(grid), len(grid[0])
+
+    def nearbySeats(y, x):
+        nearby = set()
+        for iy in range(max(0, y - 1), min(Y, y + 2)):
+            for ix in range(max(0, x - 1), min(X, x + 2)):
+                if ix == x and iy == y:
+                    continue
+                if grid[iy][ix] != FLOOR:
+                    nearby.add((iy, ix))
+        return nearby
+
+    def rule(occupied, nearbyOccupied):
+        if occupied:
+            return EMPTY if nearbyOccupied >= 4 else OCCUPIED
+        else:
+            return EMPTY if nearbyOccupied > 0 else OCCUPIED
+
+    coords = ((y, x) for y in range(Y) for x in range(X))
+    connectedSeats = {c: nearbySeats(*c) for c in coords if grid[c[0]][c[1]] != FLOOR}
 
     seen = set()
     occupiedSeats = 0
-
     while occupiedSeats not in seen:
         seen.add(occupiedSeats)
-        grid, occupiedSeats = tick(grid)
+        grid, occupiedSeats = tick(grid, rule, connectedSeats)
 
     return occupiedSeats
 
 
 def part2(data):
-    grid = data.splitlines()
+    grid = [list(row) for row in data.splitlines()]
+    Y, X = len(grid), len(grid[0])
+
+    def nearbySeats(y, x):
+        nearby = set()
+        for dy in (-1, 0, 1):
+            for dx in (-1, 0, 1):
+                if dx == 0 and dy == 0:
+                    continue
+                iy = y + dy
+                ix = x + dx
+                while 0 <= iy < Y and 0 <= ix < X:
+                    if grid[iy][ix] == FLOOR:
+                        iy += dy
+                        ix += dx
+                        continue
+                    else:
+                        nearby.add((iy, ix))
+                        break
+        return nearby
+
+    def rule(occupied, nearbyOccupied):
+        if occupied:
+            return EMPTY if nearbyOccupied >= 5 else OCCUPIED
+        else:
+            return EMPTY if nearbyOccupied > 0 else OCCUPIED
+
+    coords = ((y, x) for y in range(Y) for x in range(X))
+    connectedSeats = {c: nearbySeats(*c) for c in coords if grid[c[0]][c[1]] != FLOOR}
 
     seen = set()
     occupiedSeats = 0
-
     while occupiedSeats not in seen:
         seen.add(occupiedSeats)
-        # print(occupiedSeats)
-        # print(*grid, sep='\n')
-        # print('\n---------\n')
-        grid, occupiedSeats = tick(grid, floorOk=False)
+        grid, occupiedSeats = tick(grid, rule, connectedSeats)
 
     return occupiedSeats
