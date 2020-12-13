5b148604af9373c46c345cfbd4019a0892ff08e5
Ted Cassirer <ted.cassirer@gmail.com>
2020-12-13 11:11:05 +0100

    Day 13

---
+++ aoc_cas/aoc2020/day13.py
@@ -0,0 +1,17 @@
+def part1(data):
+    earliest, schedules = data.splitlines()
+    earliest = int(earliest)
+    schedules = [int(busId) for busId in schedules.split(",") if busId != "x"]
+    busToTake = min(schedules, key=lambda busId: (-earliest % busId))
+    return busToTake * (-earliest % busToTake)
+
+
+def part2(data):
+    schedule = [(i, int(busId)) for i, busId in enumerate(data.splitlines()[1].split(",")) if busId != "x"]
+    jump = 1
+    time = 0
+    for offset, busId in schedule:
+        while (time + offset) % busId != 0:
+            time += jump
+        jump *= busId
+    return time
