446cb6f8d3c0faed329dd4f2b772170bd2c83b9d
Ted Cassirer <ted.cassirer@gmail.com>
2020-12-16 09:43:55 +0100

    Day 16

---
+++ aoc_cas/aoc2020/day16.py
@@ -0,0 +1,58 @@
+import re
+
+ruleMatch = re.compile(r"(.+): (\d+-\d+) or (\d+-\d+)")
+
+
+def parseRules(rules):
+    allowedNumbers = []
+    for r in rules.splitlines():
+        name, r1, r2 = ruleMatch.match(r).groups()
+        r1a, r1b = map(int, r1.split("-"))
+        r2a, r2b = map(int, r2.split("-"))
+        allowedNumbers.append((name, set(range(r1a, r1b + 1)) | set(range(r2a, r2b + 1))))
+    return allowedNumbers
+
+
+def part1(data):
+    rules, myTicket, nearbyTickets = data.split("\n\n")
+    rules = parseRules(rules)
+    allowedNumbers = set.union(*(r for name, r in rules))
+    nearbyTickets = nearbyTickets.splitlines()[1:]
+    result = 0
+
+    for tickets in nearbyTickets:
+        tickets = [int(t) for t in tickets.split(",")]
+        result += sum(t for t in tickets if t not in allowedNumbers)
+
+    return result
+
+
+def part2(data):
+    rules, myTicket, nearbyTickets = data.split("\n\n")
+    myTicket = [int(n) for n in myTicket.split("\n")[1].split(",")]
+    rules = parseRules(rules)
+    allowedNumbers = set.union(*(r for name, r in rules))
+
+    nearbyTickets = nearbyTickets.splitlines()[1:]
+    validTickets = []
+    for tickets in nearbyTickets:
+        tickets = [int(t) for t in tickets.split(",")]
+        if all(t in allowedNumbers for t in tickets):
+            validTickets.append(tickets)
+
+    possibleRuleIndex = {name: set() for name, r in rules}
+    for i, n in enumerate(zip(*validTickets)):
+        for name, rule in rules:
+            if all(t in rule for t in n):
+                possibleRuleIndex[name].add(i)
+
+    taken = set()
+    res = 1
+    for name, possible in sorted(possibleRuleIndex.items(), key=lambda r: len(r[1])):
+        possible -= taken
+        assert len(possible) == 1
+        i = possible.pop()
+        taken.add(i)
+        if name.startswith("departure"):
+            res *= myTicket[i]
+    return res
---
+++ test/2020/test_2020_day16.py
@@ -0,0 +1,36 @@
+from aoc_cas.aoc2020 import day16 as aoc
+
+
+def testPart1():
+    data = """
+class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12
+""".strip()
+    assert aoc.part1(data) == 71
+
+
+def testPart2():
+    data = """
+departure something: 0-1 or 4-19
+row: 0-5 or 8-19
+departure something else: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+""".strip()
+    assert aoc.part2(data) == 12 * 13
