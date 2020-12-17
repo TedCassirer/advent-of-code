6d57376e13d316e45f3c893145498bee48167db6
Ted Cassirer <ted.cassirer@gmail.com>
2020-12-17 10:13:12 +0100

    Day 17

---
+++ aoc_cas/aoc2020/day17.py
@@ -0,0 +1,40 @@
+from collections import Counter
+from itertools import product
+
+
+def getBuddies(coord):
+    for diffVals in product((-1, 0, 1), repeat=len(coord)):
+        if any(diffVals):
+            yield tuple(c + dv for c, dv in zip(coord, diffVals))
+
+
+def gameOfCube(activeCubes):
+    coordsWithActiveBuddies = Counter(buddy for coord in activeCubes for buddy in getBuddies(coord))
+    newActiveCubes = set()
+    for coord, activeBuddies in coordsWithActiveBuddies.items():
+        if activeBuddies == 3 or (coord in activeCubes and activeBuddies == 2):
+            newActiveCubes.add(coord)
+    return newActiveCubes
+
+
+def createInitialState(data, dimensions):
+    activeCubes = set()
+    for x, line in enumerate(data.splitlines()):
+        for y, val in enumerate(line):
+            if val == "#":
+                activeCubes.add((x, y) + (0,) * (dimensions - 2))
+    return activeCubes
+
+
+def part1(data):
+    activeCubes = createInitialState(data, 3)
+    for i in range(6):
+        activeCubes = gameOfCube(activeCubes)
+    return len(activeCubes)
+
+
+def part2(data):
+    activeCubes = createInitialState(data, 4)
+    for i in range(6):
+        activeCubes = gameOfCube(activeCubes)
+    return len(activeCubes)
