8629bfed3bb62ca146eb0161181e4038445cf920
Ted Cassirer <ted.cassirer@gmail.com>
2020-12-20 20:07:49 +0100

    Day 20 cleaned up a bit

--- aoc_cas/aoc2020/day20.py
+++ aoc_cas/aoc2020/day20.py
@@ -1,4 +1,3 @@
-from functools import cached_property
 from dataclasses import dataclass
 from typing import Tuple
 from collections import defaultdict
@@ -7,94 +6,71 @@ from collections import defaultdict
 @dataclass(frozen=True, eq=True)
 class Tile:
     id: int
-    tileString: Tuple[Tuple[str]]
-    up: Tuple[str]
-    down: Tuple[str]
-    left: Tuple[str]
-    right: Tuple[str]
-    rotations: int
-    flipped: bool
+    data: Tuple[Tuple[str]]
 
     @staticmethod
     def createFromData(tileData):
-        tileId, *tileString = tileData.splitlines()
+        tileId, *data = tileData.splitlines()
         tileId = int(tileId.split()[-1][:-1])
-        up = tuple(tileString[0])
-        down = tuple(tileString[-1])
-        left = tuple(row[0] for row in tileString)
-        right = tuple(row[-1] for row in tileString)
-        return Tile(
-            tileId,
-            tileString=tuple(map(tuple, tileString)),
-            up=up,
-            right=right,
-            down=down,
-            left=left,
-            rotations=0,
-            flipped=False,
-        )
+        return Tile(tileId, data=tuple(map(tuple, data)))
 
-    @cached_property
+    @property
+    def up(self):
+        return tuple(self.data[0])
+
+    @property
+    def right(self):
+        return tuple(row[-1] for row in self.data)
+
+    @property
+    def down(self):
+        return tuple(self.data[-1])
+
+    @property
+    def left(self):
+        return tuple(row[0] for row in self.data)
+
     def rotate(self):
-        return Tile(
-            self.id,
-            tileString=self.tileString,
-            up=self.left[::-1],
-            right=self.up,
-            down=self.right[::-1],
-            left=self.down,
-            rotations=self.rotations + 1,
-            flipped=self.flipped,
-        )
+        return Tile(self.id, data=rotateMatrix(self.data))
 
-    @cached_property
     def flip(self):
-        return Tile(
-            self.id,
-            tileString=self.tileString,
-            up=self.up[::-1],
-            right=self.left,
-            down=self.down[::-1],
-            left=self.right,
-            rotations=self.rotations,
-            flipped=not self.flipped,
-        )
+        return Tile(self.id, data=flipMatrix(self.data))
 
-    @cached_property
     def variations(self):
-        variations = []
         tile = self
-        variations.append(tile)
-        variations.append(tile.flip)
+        yield tile
+        yield tile.flip()
         for _ in range(3):
-            tile = tile.rotate
-            variations.append(tile)
-            variations.append(tile.flip)
-        return variations
+            tile = tile.rotate()
+            yield tile
+            yield tile.flip()
 
-    def tileData(self):
-        output = [r[1:-1] for r in self.tileString[1:-1]]
-        for _ in range(self.rotations):
-            output = list(zip(*output[::-1]))
-        if self.flipped:
-            output = [r[::-1] for r in output]
-        return output
+    def stripBorders(self):
+        return [r[1:-1] for r in self.data[1:-1]]
+
+
+def flipMatrix(matrix):
+    return matrix[::-1]
+
+
+def rotateMatrix(matrix):
+    return tuple(zip(*matrix[::-1]))
 
 
 def tileItUp(tiles):
     N = int(len(tiles) ** 0.5)
     assert N ** 2 == len(tiles)
 
-    allTiles = frozenset(tileVariation for tile in tiles for tileVariation in tile.variations)
-    tileIndex = defaultdict(list)
+    allTiles = frozenset(tileVariation for tile in tiles for tileVariation in tile.variations())
+    borderIndex = defaultdict(list)
     for tile in allTiles:
-        tileIndex[tile.left].append(tile)
+        borderIndex[tile.left].append(tile)
 
     def findRows(row, blackList=frozenset()):
         if len(row) == N:
             yield row
             return
-        candidates = tileIndex[row[-1].right] if row else allTiles
+        candidates = borderIndex[row[-1].right] if row else allTiles
         for nextTile in candidates:
             if nextTile.id in blackList:
                 continue
@@ -103,6 +79,7 @@ def tileItUp(tiles):
     def stackRows(stack, remainingRows):
         if len(stack) == N:
             yield stack
+            return
         prevRow = stack[-1] if stack else frozenset()
         seenIds = {t.id for row in stack for t in row}
         for row in remainingRows:
@@ -112,30 +89,30 @@ def tileItUp(tiles):
                 yield from stackRows(stack + (row,), remainingRows - {row})
 
     rows = frozenset(findRows(tuple(), frozenset()))
-    grids = stackRows(tuple(), rows)
-    return grids
+    yield from stackRows(tuple(), rows)
 
 
 def mergeTiles(grid):
     image = []
     for row in grid:
-        lines = zip(*(t.tileData() for t in row))
+        lines = zip(*(t.stripBorders() for t in row))
         lines = tuple(tuple(l for subline in line for l in subline) for line in lines)
         image.extend(lines)
     return image
 
 
 def countSeaMonsters(image):
-    tops = [18]
-    mids = [0, 5, 6, 11, 12, 17, 18, 19]
-    bots = [1, 4, 7, 10, 13, 16]
+    # Indices for every # in the seamonster
+    top = [18]
+    mid = [0, 5, 6, 11, 12, 17, 18, 19]
+    bot = [1, 4, 7, 10, 13, 16]
     monsters = 0
     for r1, r2, r3 in zip(image, image[1:], image[2:]):
         for i in range(len(r2) - 20):
             if (
-                all(r1[i + t] == "#" for t in tops)
-                and all(r2[i + m] == "#" for m in mids)
-                and all(r3[i + b] == "#" for b in bots)
+                all(r1[i + t] == "#" for t in top)
+                and all(r2[i + m] == "#" for m in mid)
+                and all(r3[i + b] == "#" for b in bot)
             ):
                 monsters += 1
     return monsters
@@ -153,17 +130,18 @@ def part1(data):
 
 
 def part2(data):
-    seamonster = """
+    seaMonster = """
                       # 
     #    ##    ##    ###
      #  #  #  #  #  #   
     """
     tiles = getTiles(data)
-    for grid in tileItUp(tiles):
-        image = mergeTiles(grid)
-        monsters = countSeaMonsters(image)
-        if monsters:
-            return sum(l.count("#") for l in image) - seamonster.count("#") * monsters
+    grid = next(tileItUp(tiles))
+    image = mergeTiles(grid)
+    monsters = countSeaMonsters(image)
+    while not (monsters := countSeaMonsters(image) or countSeaMonsters(flipMatrix(image))):
+        image = rotateMatrix(image)
+    return sum(l.count("#") for l in image) - seaMonster.count("#") * monsters
 
 
 if __name__ == "__main__":
