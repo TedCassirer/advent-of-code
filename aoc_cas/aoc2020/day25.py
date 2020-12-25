6bb0c65841692d552a318ec56c623bc5156792fe
Ted Cassirer <ted.cassirer@gmail.com>
2020-12-25 11:58:25 +0100

    Day 25

---
+++ aoc_cas/aoc2020/day25.py
@@ -0,0 +1,28 @@
+N = 20201227
+
+
+def calculateLoopSize(publicKey):
+    loopSize = 0
+    n = 1
+    while n != publicKey:
+        n = (n * 7) % N
+        loopSize += 1
+    return loopSize
+
+
+def calculateEncryptionKey(publicKey, loopSize):
+    n = 1
+    for _ in range(loopSize):
+        n = (n * publicKey) % N
+    return n
+
+
+def part1(data):
+    cardPublicKey, doorPublicKey = map(int, data.splitlines())
+    doorLoopSize = calculateLoopSize(doorPublicKey)
+    encryptionKey = calculateEncryptionKey(cardPublicKey, doorLoopSize)
+    return encryptionKey
+
+
+def part2(data):
+    pass
