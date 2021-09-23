7d62ffa86a747430db64e80dbdee39f38f88a6e0
Ted Cassirer <tedc@spotify.com>
2021-09-23 18:13:24 +0200

    2015 day 9

---
+++ aoc_cas/aoc2015/day9.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+import heapq
+
+
+def buildGraph(data):
+    graph = defaultdict(list)
+    for line in data.splitlines():
+        city1, _, city2, _, dist = line.split(" ")
+        dist = int(dist)
+        graph[city1].append((city2, dist))
+        graph[city2].append((city1, dist))
+    return graph
+
+
+def search(start, graph, longest=False):
+    queue = [(0, start, {start})]
+    sign = -1 if longest else 1
+    while queue:
+        currDist, current, visited = heapq.heappop(queue)
+        currDist *= sign
+        if len(visited) == len(graph):
+            return currDist
+        for nextCity, dist in graph[current]:
+            if nextCity not in visited:
+                heapq.heappush(queue, (sign * (currDist + dist), nextCity, visited | {nextCity}))
+
+
+def part1(data):
+    graph = buildGraph(data)
+    return min(search(start, graph) for start in graph.keys())
+
+
+def part2(data):
+    graph = buildGraph(data)
+    return max(search(start, graph, longest=True) for start in graph.keys())
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2015, day=9)
+    print(part1(data))
+    print(part2(data))
