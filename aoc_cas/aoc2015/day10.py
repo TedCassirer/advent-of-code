3ed559b963f8bb7ffe138f2b4a547547b6bdd0a9
Ted Cassirer <tedc@spotify.com>
2021-09-23 18:23:55 +0200

    2015 day 10

---
+++ aoc_cas/aoc2015/day10.py
@@ -0,0 +1,32 @@
+def countGame(number):
+    streak = 1
+    out = ""
+    for n1, n2 in zip(number, number[1:]):
+        if n1 == n2:
+            streak += 1
+        else:
+            out += f"{streak}{n1}"
+            streak = 1
+    return f"{out}{streak}{n2}"
+
+
+def part1(data):
+    number = data
+    for _ in range(40):
+        number = countGame(number)
+    return len(number)
+
+
+def part2(data):
+    number = data
+    for _ in range(50):
+        number = countGame(number)
+    return len(number)
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2015, day=10)
+    print(part1(data))
+    print(part2(data))
