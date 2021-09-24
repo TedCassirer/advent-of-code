94b501309553d2f25770e308adb86a2355d0c67a
Ted Cassirer <tedc@spotify.com>
2021-09-24 14:46:53 +0200

    2015 day 15

---
+++ aoc_cas/aoc2015/day15.py
@@ -0,0 +1,45 @@
+import numpy as np
+from functools import lru_cache
+
+
+@lru_cache(None)
+def amounts(N, hi):
+    if N == 1:
+        return [[hi]]
+    else:
+        out = []
+        for v1 in range(1, hi - N):
+            p1 = [v1]
+            for p2 in amounts(N - 1, hi - v1):
+                out.append(p1 + p2)
+        return out
+
+
+def parse(line):
+    return np.array([int(w.split(" ")[-1]) for w in line.split(", ")])
+
+
+def part1(data):
+    ingredients = np.array([parse(line) for line in data.splitlines()]).T
+    recipes = np.array(amounts(ingredients.shape[1], 100)).T
+    cakes = np.matmul(ingredients, recipes)
+    cakes[-1, :] = 1
+    cakes = np.maximum(cakes, 0)
+    return max(np.prod(cakes, 0))
+
+
+def part2(data):
+    ingredients = np.array([parse(line) for line in data.splitlines()]).T
+    recipes = np.array(amounts(ingredients.shape[1], 100)).T
+    cakes = np.matmul(ingredients, recipes)
+    cakes[-1, :] = cakes[-1, :] == 500
+    cakes = np.maximum(cakes, 0)
+    return max(np.prod(cakes, 0))
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2015, day=15)
+    print(part1(data))
+    print(part2(data))
