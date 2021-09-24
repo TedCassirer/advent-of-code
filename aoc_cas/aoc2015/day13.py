1db303f78be49c09917e258b917fc182872a90c5
Ted Cassirer <tedc@spotify.com>
2021-09-24 10:47:14 +0200

    2015 day 13

---
+++ aoc_cas/aoc2015/day13.py
@@ -0,0 +1,46 @@
+import itertools
+from collections import defaultdict
+
+
+def parse(line):
+    words = line.split(" ")
+    p1, happiness, p2 = words[0], int(words[3]), words[-1][:-1]
+    if words[2] == "lose":
+        happiness *= -1
+    return (p1, happiness, p2)
+
+
+def getScoring(data):
+    scoring = defaultdict(lambda: defaultdict(int))
+    for p1, happiness, p2 in map(parse, data.splitlines()):
+        scoring[p1][p2] = happiness
+    return scoring
+
+
+def score(arangement, scoring):
+    score = 0
+    for i, n in enumerate(arangement):
+        score += scoring[n][arangement[i - 1]]
+        score += scoring[n][arangement[(i + 1) % len(arangement)]]
+    return score
+
+
+def part1(data):
+    scoring = getScoring(data)
+    people = scoring.keys()
+    return max(score(arrangement, scoring) for arrangement in itertools.permutations(people))
+
+
+def part2(data):
+    scoring = getScoring(data)
+    people = list(scoring.keys())
+    people.append("ME!!!")
+    return max(score(arrangement, scoring) for arrangement in itertools.permutations(people))
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2015, day=13)
+    print(part1(data))
+    print(part2(data))
