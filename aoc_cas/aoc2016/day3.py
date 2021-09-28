55d35472ea69486c017fd8dc8282cf08121a55ce
Ted Cassirer <tedc@spotify.com>
2021-09-28 13:46:31 +0200

    2016 day 3

---
+++ aoc_cas/aoc2016/day3.py
@@ -0,0 +1,42 @@
+import re
+
+
+def parseDataRow(data):
+    for line in data.splitlines():
+        yield tuple(int(n) for n in re.findall(r"\d+", line))
+
+
+def parseDataColumn(data):
+    rowData = list(n for row in parseDataRow(data) for n in row)
+    n1 = rowData[0::3]
+    n2 = rowData[1::3]
+    n3 = rowData[2::3]
+    byColumn = n1 + n2 + n3
+    return zip(byColumn[0::3], byColumn[1::3], byColumn[2::3])
+
+
+def checkLengths(n1, n2, n3):
+    return n1 + n2 > n3
+
+
+def part1(data):
+    valid = 0
+    for nums in parseDataRow(data):
+        valid += checkLengths(*sorted(nums))
+    return valid
+
+
+def part2(data):
+    valid = 0
+    for nums in parseDataColumn(data):
+        valid += checkLengths(*sorted(nums))
+    return valid
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2016, day=3)
+
+    print(part1(data))
+    print(part2(data))
