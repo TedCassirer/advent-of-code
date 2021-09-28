9ddded74dd388d175dbd36147d6bf738c9a96092
Ted Cassirer <tedc@spotify.com>
2021-09-28 12:57:22 +0200

    2016 day 2

---
+++ aoc_cas/aoc2016/day2.py
@@ -0,0 +1,50 @@
+def getPassWord(numpad, data, startCol, startRow):
+    c, r = startCol, startRow
+    pw = ""
+    for line in data.splitlines():
+        for i in line:
+            if i == "R" and numpad[r][c + 1] != ".":
+                c += 1
+            elif i == "L" and numpad[r][c - 1] != ".":
+                c -= 1
+            elif i == "D" and numpad[r + 1][c] != ".":
+                r += 1
+            elif i == "U" and numpad[r - 1][c] != ".":
+                r -= 1
+        pw += numpad[r][c]
+    return pw
+
+
+def part1(data):
+    numpad = """
+.....
+.123.
+.456.
+.789.
+.....
+""".strip().splitlines()
+    startCol, startRow = 2, 2
+    return getPassWord(numpad, data, startCol, startRow)
+
+
+def part2(data):
+    numpad = """
+.......
+...1...
+..234..
+.56789.
+..ABC..
+...D...
+.......
+""".strip().splitlines()
+    startCol, startRow = 1, 3
+    return getPassWord(numpad, data, startCol, startRow)
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2016, day=2)
+
+    print(part1(data))
+    print(part2(data))
