4c083c2cdbdb0fcab7e4a775fe8d987cdb08cca2
Ted Cassirer <tedc@spotify.com>
2021-09-29 12:04:22 +0200

    2016 day 9

---
+++ aoc_cas/aoc2016/day9.py
@@ -0,0 +1,32 @@
+import re
+
+
+def decompressLength(string, recursive):
+    expansion = re.search(r"\((\d+x\d+)\)", string)
+    if expansion:
+        start, end = expansion.span()
+        a, b = map(int, string[start + 1 : end - 1].split("x"))
+        p1 = string[:start]
+        toRepeat = string[end : end + a]
+        p3 = string[end + a :]
+        if recursive:
+            return len(p1) + decompressLength(toRepeat, recursive) * b + decompressLength(p3, recursive)
+        else:
+            return len(p1) + len(toRepeat) * b + decompressLength(p3, recursive)
+    return len(string)
+
+
+def part1(data):
+    return decompressLength(data, recursive=False)
+
+
+def part2(data):
+    return decompressLength(data, recursive=True)
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2016, day=9)
+    print(part1(data))
+    print(part2(data))
