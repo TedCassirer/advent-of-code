da28bc42bc5806dec338bb84b7b9afea9dc9a0df
Ted Cassirer <tedc@spotify.com>
2021-11-24 21:41:49 +0700

    Prepared for 2021

---
+++ aoc_cas/aoc2021/template.py
@@ -0,0 +1,15 @@
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2016, day=0)
+
+    print(part1(data))
+    print(part2(data))
