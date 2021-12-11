35044e611857d450d07b80293e75af1aeb255040
Ted Cassirer <tedc@spotify.com>
2021-12-11 19:23:00 +0100

    Year 2021 day 9

---
+++ aoc_cas/aoc2021/day9.py
@@ -0,0 +1,61 @@
+def getGrid(data):
+    return [[int(n) for n in row] for row in data.splitlines()]
+
+
+def lowPoints(grid):
+    M, N = len(grid), len(grid[0])
+    for y, row in enumerate(grid):
+        for x, n in enumerate(row):
+            right = grid[y][x + 1] if x != N - 1 else 9
+            up = grid[y - 1][x] if y != 0 else 9
+            left = grid[y][x - 1] if x != 0 else 9
+            down = grid[y + 1][x] if y != M - 1 else 9
+            if n < min((right, up, left, down)):
+                yield y, x
+
+
+def bfs(grid, y, x):
+    if grid[y][x] == 9:
+        return []
+
+    M, N = len(grid), len(grid[0])
+
+    basin = set()
+    stack = [(y, x)]
+    while stack:
+        y, x = stack.pop()
+        if (y, x) in basin or y < 0 or y == M or x < 0 or x == N or grid[y][x] == 9:
+            continue
+        basin.add((y, x))
+        stack.extend([(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)])
+    return basin
+
+
+def part1(data):
+    grid = getGrid(data)
+    return sum(1 + grid[y][x] for y, x in lowPoints(grid))
+
+
+def part2(data):
+    grid = getGrid(data)
+    M, N = len(grid), len(grid[0])
+    basins = []
+    seen = set()
+    for y in range(M):
+        for x in range(N):
+            if (y, x) in seen:
+                continue
+            basin = bfs(grid, y, x)
+            basins.append(len(basin))
+            seen.update(basin)
+    b1, b2, b3 = sorted(basins, reverse=True)[:3]
+    return b1 * b2 * b3
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2021, day=9)
+
+    print(part1(data))
+    print(part2(data))
---
+++ test/2021/test_2021_day9.py
@@ -0,0 +1,20 @@
+import aoc_cas.aoc2021.day9 as aoc
+
+testData = """
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+""".strip()
+
+
+def testPart1():
+    assert aoc.part1(testData) == 15
+
+
+def testPart2():
+    assert aoc.part2(testData) == 1134
+
+
+testPart2()
