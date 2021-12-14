06ab4767a5f478635bd643963356e047834f1b95
Ted Cassirer <tedc@spotify.com>
2021-12-14 09:08:16 +0100

    Year 2021 day 14

---
+++ aoc_cas/aoc2021/day14.py
@@ -0,0 +1,54 @@
+from collections import Counter
+
+
+def getRules(data):
+    _, rulesString = data.split("\n\n")
+    rules = dict()
+    for line in rulesString.splitlines():
+        k, v = line.split(" -> ")
+        rules[k] = v
+    return rules
+
+
+def getPolymer(data):
+    polymerString = data.splitlines()[0]
+    polymer = Counter(c1 + c2 for c1, c2 in zip(polymerString, polymerString[1:]))
+    charCount = Counter(polymerString)
+    return polymer, charCount
+
+
+def insertPairs(polymer, charCount, rules):
+    polymerOut = Counter()
+    for pair, count in polymer.items():
+        c1, c3 = pair
+        c2 = rules[pair]
+        polymerOut[c1 + c2] += count
+        polymerOut[c2 + c3] += count
+        charCount[c2] += count
+
+    return polymerOut, charCount
+
+
+def part1(data):
+    polymer, charCount = getPolymer(data)
+    rules = getRules(data)
+    for _ in range(10):
+        polymer, charCount = insertPairs(polymer, charCount, rules)
+    return max(charCount.values()) - min(charCount.values())
+
+
+def part2(data):
+    polymer, charCount = getPolymer(data)
+    rules = getRules(data)
+    for _ in range(40):
+        polymer, charCount = insertPairs(polymer, charCount, rules)
+    return max(charCount.values()) - min(charCount.values())
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2021, day=14)
+
+    print(part1(data))
+    print(part2(data))
