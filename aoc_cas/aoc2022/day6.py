d7051310872c18a731e656612e023a8afbea1917
Ted Cassirer <tedc@spotify.com>
2022-12-06 08:18:06 +0100

    2022 Day 6

---
+++ aoc_cas/aoc2022/day6.py
@@ -0,0 +1,21 @@
+def part1(data):
+    for i in range(4, len(data)):
+        if len(set(data[i - 4 : i])) == 4:
+            return i
+    raise ValueError(":(")
+
+
+def part2(data):
+    for i in range(14, len(data)):
+        if len(set(data[i - 14 : i])) == 14:
+            return i
+    raise ValueError(":(")
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2022, day=6)
+
+    print(part1(data))
+    print(part2(data))
