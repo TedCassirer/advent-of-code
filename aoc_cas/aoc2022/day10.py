50260e8da60f6c62dcc4554c7d68df78556cfbb3
Ted Cassirer <tedc@spotify.com>
2023-01-02 17:42:02 +0700

    2022 Day 10

---
+++ aoc_cas/aoc2022/day10.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Circuit:
+    register: int = 1
+
+    def noop(self):
+        yield self.register
+
+    def addx(self, v):
+        yield self.register
+        yield self.register
+        self.register += v
+
+    def runInstructions(self, instructions):
+        for instruction in instructions.splitlines():
+            if instruction == "noop":
+                yield from self.noop()
+            else:
+                _, v = instruction.split(" ")
+                yield from self.addx(int(v))
+
+
+def part1(data):
+    circuit = Circuit()
+    out = 0
+    for i, strength in enumerate(circuit.runInstructions(data)):
+        if ((i + 1) % 40) == 20:
+            out += strength * (i + 1)
+    return out
+
+
+def part2(data):
+    circuit = Circuit()
+    crt = [["."] * 40 for _ in range(6)]
+    for i, strength in enumerate(circuit.runInstructions(data)):
+        x = i % 40
+        y = i // 40
+        if abs(x - strength) <= 1:
+            crt[y][x] = "#"
+
+    display = "\n".join(("".join(row) for row in crt))
+    print()
+    print(display)
+    expected = """
+####..##..#....#..#.###..#....####...##.
+#....#..#.#....#..#.#..#.#....#.......#.
+###..#....#....####.###..#....###.....#.
+#....#.##.#....#..#.#..#.#....#.......#.
+#....#..#.#....#..#.#..#.#....#....#..#.
+####..###.####.#..#.###..####.#.....##..
+""".strip()
+
+    if display == expected:
+        return "EGLHBLFJ"
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2022, day=10)
+
+    print(part1(data))
+    print(part2(data))
