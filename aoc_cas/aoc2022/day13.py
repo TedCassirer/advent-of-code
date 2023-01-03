0762a6e763366c0189e29a9dbc798002b5d7650e
Ted Cassirer <tedc@spotify.com>
2023-01-03 17:59:58 +0700

    2022 Day 13

---
+++ aoc_cas/aoc2022/day13.py
@@ -0,0 +1,63 @@
+from itertools import zip_longest
+from functools import cmp_to_key
+
+
+def compare(p1, p2):
+    if p1 == None:
+        return -1
+    if p2 == None:
+        return 1
+    if isinstance(p1, int):
+        if isinstance(p2, int):
+            if p1 < p2:
+                return -1
+            if p1 == p2:
+                return 0
+            else:
+                return 1
+        else:
+            return compare([p1], p2)
+    else:
+        if isinstance(p2, int):
+            return compare(p1, [p2])
+        else:
+            for i1, i2 in zip_longest(p1, p2):
+                if (compareRes := compare(i1, i2)) != 0:
+                    return compareRes
+            return 0
+
+
+def parsePackets(packets):
+    for packetPair in packets.split("\n\n"):
+        p1, p2 = map(eval, packetPair.splitlines())
+        yield p1, p2
+
+
+def part1(data):
+    _sum = 0
+    for i, (p1, p2) in enumerate(parsePackets(data)):
+        if compare(p1, p2) <= 0:
+            _sum += i + 1
+    return _sum
+
+
+def part2(data):
+    packets = [packet for packetPair in parsePackets(data) for packet in packetPair]
+    dividerPacket1 = [[2]]
+    dividerPacket2 = [[6]]
+    packets.append(dividerPacket1)
+    packets.append(dividerPacket2)
+
+    packets.sort(key=cmp_to_key(compare))
+    p1Index = packets.index(dividerPacket1) + 1
+    p2Index = packets.index(dividerPacket2) + 1
+    return p1Index * p2Index
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2022, day=13)
+
+    print(part1(data))
+    print(part2(data))
---
+++ test/2022/test_2022_day13.py
@@ -0,0 +1,40 @@
+import aoc_cas.aoc2022.day13 as aoc
+
+testData = """
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+""".strip()
+
+
+def testPart1():
+    assert aoc.part1(testData) == 13
+
+
+def testPart2():
+    assert aoc.part2(testData) == 140
+
+
+if __name__ == "__main__":
+    testPart1()
+    testPart2()
