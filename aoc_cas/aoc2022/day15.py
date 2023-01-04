9b38259baf71326bb9102858a332922675f9d50a
Ted Cassirer <tedc@spotify.com>
2023-01-04 18:12:41 +0700

    2022 Day 15 slow

---
+++ aoc_cas/aoc2022/day15.py
@@ -0,0 +1,62 @@
+import re
+
+REGEX = re.compile(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
+
+
+def parseSensorsAndBeacons(data):
+    for line in data.splitlines():
+        sx, sy, bx, by = map(int, REGEX.search(line).groups())
+        yield (sy, sx), (by, bx)
+
+
+def manhattanDistance(p1, p2):
+    return abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])
+
+
+def part1(data, row=2000000):
+    noBeacon = set()
+    onRow = set()
+    for sensor, beacon in parseSensorsAndBeacons(data):
+        dist = manhattanDistance(sensor, beacon)
+        distToRow = abs(sensor[0] - row)
+        if distToRow > dist:
+            continue
+        width = dist - distToRow
+        noBeacon.update(range(sensor[1] - width, sensor[1] + width + 1))
+        if sensor[0] == row:
+            onRow.add(sensor[1])
+        if beacon[0] == row:
+            onRow.add(beacon[1])
+    return len(noBeacon) - len(onRow)
+
+
+def part2(data, searchSpace=4000000):
+    rows = [[(-1, -1), (searchSpace + 1, searchSpace + 1)] for _ in range(searchSpace + 1)]
+    for sensor, beacon in parseSensorsAndBeacons(data):
+        dist = manhattanDistance(sensor, beacon)
+        top = max(0, sensor[0] - dist)
+        bottom = min(searchSpace, sensor[0] + dist)
+        for row in range(top, bottom + 1):
+            width = dist - abs(row - sensor[0])
+            left = max(0, sensor[1] - width)
+            right = min(searchSpace + 1, sensor[1] + width)
+            rows[row].append((left, right))
+
+    for y, row in enumerate(rows):
+        row.sort()
+        x = 0
+        for l, r in row:
+            if r <= x:
+                continue
+            if x + 1 < l:
+                return (x + 1) * 4000000 + y
+            x = max(x, r)
+
+
+if __name__ == "__main__":
+    from aocd import get_data
+
+    data = get_data(year=2022, day=15)
+
+    print(part1(data))
+    print(part2(data))
