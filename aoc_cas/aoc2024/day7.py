2792e8d5a93a9aa7acceb8bb5a52ca4126b31895
Ted Cassirer <ted.cassirer1@ocado.com>
2024-12-07 12:16:11 +0700

    2024 day 7

---
+++ aoc_cas/aoc2024/day7.py
@@ -0,0 +1,44 @@
+from operator import add, mul
+import typing as t
+
+Operator = t.Callable[[int, int], int]
+
+
+def _parse_input(data: str) -> t.Iterator[tuple[int, list[int]]]:
+    for line in data.splitlines():
+        val, nums = line.split(": ")
+        yield int(val), [int(n) for n in nums.split(" ")]
+
+
+def _can_reach_val(val: int, nums: list[int], ops: list[Operator]) -> bool:
+    def inner(i: int, curr: int) -> bool:
+        if i == len(nums):
+            return curr == val
+        if curr > val:
+            return False
+        return any(inner(i + 1, op(curr, nums[i])) for op in ops)
+
+    return inner(1, nums[0])
+
+
+def part_a(data: str) -> int:
+    val_sum = 0
+    for val, nums in _parse_input(data):
+        if _can_reach_val(val, nums, [add, mul]):
+            val_sum += val
+    return val_sum
+
+
+def part_b(data: str) -> int:
+    concatenate: Operator = lambda a, b: int(f"{a}{b}")
+    val_sum = 0
+    for val, nums in _parse_input(data):
+        if _can_reach_val(val, nums, [add, mul, concatenate]):
+            val_sum += val
+    return val_sum
+
+
+if __name__ == "__main__":
+    from aoc_cas.util import solve_with_example_data
+
+    solve_with_example_data(year=2024, day=7)
